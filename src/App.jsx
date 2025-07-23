32b1c225b81dbd362f8dab316b5a60adf2a94123
Kero Sobhy <kerosobhy64@gamil.com>
2025-07-23 16:11:53 +0300

    لللللل

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route, Router, HashRouter } from 'react-router-dom';
+import {Routes, Route,HashRouter } from 'react-router-dom';
 import Timer from './Timer'
 import Home from './Home'
 import Html from './Html';
