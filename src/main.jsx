274733513ab87198b75ef89416dc85385865c79b
Kero Sobhy <kerosobhy64@gamil.com>
2025-07-23 16:28:44 +0300

    jjjjjjjjjjj

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { HashRouter } from 'react-router-dom'; // تم الاستيراد
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter> {/* ← لازم نلف App به */}
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
